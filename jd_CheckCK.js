f1763f56928960b80dff575facfabc5316bb2a5a
x-dr <xd9722@gmail.com>
2021-11-13 21:49:23 +0800

    ck check

--- jd_CheckCK.js
+++ jd_CheckCK.js
@@ -201,14 +201,14 @@ if ($.isNode() && process.env.CHECKCK_CKNOWARNERROR) {
 						const DisableCkBody = await DisableCk(envs[i]._id);
 						if (DisableCkBody.code == 200) {
 							if ($.isNode() && WP_APP_TOKEN_ONE) {
-								await notify.sendNotifybyWxPucher(`${$.name}`, `京东账号: ${$.nickName || $.UserName2} 已失效,自动禁用成功!\n如果要继续挂机，请联系管理员重新登录账号，账号有效期为30天.`, `${$.UserName2}`);
+								await notify.sendNotifybyWxPucher(`${$.name}`, `京东账号: ${$.nickName || $.UserName2} 已失效,自动禁用成功!\n如果要继续挂机，请登录账号，账号有效期为30天.\n登录地址：jd.ookk.me`, `${$.UserName2}`);
 							}
 							console.log(`京东账号${$.index} : ${$.nickName || $.UserName2} 已失效,自动禁用成功!\n`);
 							TempDisableMessage = ReturnMessageTitle + ` (自动禁用成功!)\n`;
 							TempErrorMessage = ReturnMessageTitle + ` 已失效,自动禁用成功!\n`;
 						} else {
 							if ($.isNode() && WP_APP_TOKEN_ONE) {
-								await notify.sendNotifybyWxPucher(`${$.name}`, `京东账号: ${$.nickName || $.UserName2} 已失效!\n如果要继续挂机，请联系管理员重新登录账号，账号有效期为30天.`, `${$.UserName2}`);
+								await notify.sendNotifybyWxPucher(`${$.name}`, `京东账号: ${$.nickName || $.UserName2} 已失效!\n如果要继续挂机，请重新登录账号，账号有效期为30天.\n登录地址：jd.ookk.me`, `${$.UserName2}`);
 							}
 							console.log(`京东账号${$.index} : ${$.nickName || $.UserName2} 已失效,自动禁用失败!\n`);
 							TempDisableMessage = ReturnMessageTitle + ` (自动禁用失败!)\n`;
